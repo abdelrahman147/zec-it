15df14eb4b0cdd87aae2b5d8ea912dc4f052e3dd
abdelrahman147 <abdelrahman.mohsen1000@gmail.com>
2025-12-03 08:01:34 +0200

    Trigger new deployment to pick up env vars

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Triggering new deployment for API key check
 };
 
 export default nextConfig;
