54968d3c167c24b5f1f206b5177075c64e88e539
abdelrahman147 <abdelrahman.mohsen1000@gmail.com>
2025-12-05 09:53:02 +0200

    fix: install raw-loader and configure turbopack rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-expect-error turbo is a valid option in newer Next.js versions
+  turbo: {
+    rules: {
+      "*.md": ["raw-loader"],
+      "*.LICENSE": ["raw-loader"],
+      "*.sh": ["raw-loader"],
+      "**/node_modules/thread-stream/**/*.ts": ["raw-loader"],
+    }
+  }
 };
 
 export default nextConfig;
